45d4c55b52a3b9fc2a57e4e04bf3df9380464308
gungdekrisna <gungkrisna31@gmail.com>
2021-05-27 15:36:55 +0800

    performance and security optimize

--- core/src/main/java/com/gaspol/filmplus/core/data/source/local/room/FilmDatabase.kt
+++ core/src/main/java/com/gaspol/filmplus/core/data/source/local/room/FilmDatabase.kt
@@ -4,8 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.gaspol.filmplus.core.data.source.local.entity.MovieEntity
 
-@Database(entities = [com.gaspol.filmplus.core.data.source.local.entity.MovieEntity::class], version = 1, exportSchema = false)
+@Database(entities = [MovieEntity::class], version = 1, exportSchema = false)
 abstract class FilmDatabase : RoomDatabase() {
 
-    abstract fun filmDao(): com.gaspol.filmplus.core.data.source.local.room.FilmDao
+    abstract fun filmDao(): FilmDao
 }
